8b7c0ca4245d691f2ca557777bec9d88306dd4af
timtikh <tptikhonov@edu.hse.ru>
2023-06-17 15:19:11 +0300

    fixed some decorative problems

--- for_4/script.sh
+++ for_4/script.sh
@@ -1,10 +1,10 @@
-gcc reader4.c -o reader4.o
-gcc process4.c -o process4.o
-gcc writer4.c -o writer4.o
-gcc manager4.c -o manager4.o
+gcc reader4.c -o reader4
+gcc process4.c -o process4
+gcc writer4.c -o writer4
+gcc manager4.c -o manager4
 
-./manager4.o tests/test1.txt tests/test1_res.txt
-./manager4.o tests/test2.txt tests/test2_res.txt
-./manager4.o tests/test3.txt tests/test3_res.txt
-./manager4.o tests/test4.txt tests/test4_res.txt
-./manager4.o tests/test5.txt tests/test5_res.txt
+./manager4 tests/test1.txt tests/test1_res.txt
+./manager4 tests/test2.txt tests/test2_res.txt
+./manager4 tests/test3.txt tests/test3_res.txt
+./manager4 tests/test4.txt tests/test4_res.txt
+./manager4 tests/test5.txt tests/test5_res.txt

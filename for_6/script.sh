7e7e756b40de6b582fe373ffda0bb0a4a64f576f
timtikh <tptikhonov@edu.hse.ru>
2023-09-23 20:06:03 +0300

    preworking for_6

--- for_6/script.sh
+++ for_6/script.sh
@@ -1,11 +1,7 @@
-gcc 5-process.c -o 5-process
-rm /tmp/first-fifo & rm /tmp/second-fifo
-./5-process tests/test1.txt tests/test1_res.txt
-rm /tmp/first-fifo & rm /tmp/second-fifo
-./5-process tests/test2.txt tests/test2_res.txt
-rm /tmp/first-fifo & rm /tmp/second-fifo
-./5-process tests/test3.txt tests/test3_res.txt
-rm /tmp/first-fifo & rm /tmp/second-fifo
-./5-process tests/test4.txt tests/test4_res.txt
-rm /tmp/first-fifo & rm /tmp/second-fifo
-./5-process tests/test5.txt tests/test5_res.txt
+gcc 6-process.c -o 6-process
+
+./6-process tests/test1.txt tests/test1_res.txt
+./6-process tests/test2.txt tests/test2_res.txt
+./6-process tests/test3.txt tests/test3_res.txt
+./6-process tests/test4.txt tests/test4_res.txt
+./6-process tests/test5.txt tests/test5_res.txt
